5a55cf122a9ce5d7934477e4337e520d64a6b5b2
Jack Mousseau <jackmousseau@gmail.com>
2017-01-06 12:49:18 -0600

    Add AnnoucementsTableVC with tab bar icon

---
+++ HackCWRU/AnnouncementsTableViewController.swift
@@ -0,0 +1,103 @@
+//
+//  AnnouncementsTableViewController.swift
+//  HackCWRU
+//
+//  Created by Jack on 1/6/17.
+//  Copyright © 2017 Hacker Society. All rights reserved.
+//
+
+import UIKit
+
+class AnnouncementsTableViewController: UITableViewController {
+    
+    // MARK: - Properties
+    
+    private var sortedAnnouncements = [Announcement]()
+    
+    private var announcements = [Announcement]() {
+        didSet {
+            sortedAnnouncements = announcements.sorted { lhs, rhs in
+                // Sort newest to oldest.
+                lhs.updatedAt > rhs.updatedAt
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        addRefreshControl()
+        refresh()
+        
+        tableView.tableFooterView = UIView()
+    }
+    
+    func addRefreshControl() {
+        refreshControl = UIRefreshControl()
+        refreshControl?.addTarget(self, action: #selector(refresh), for: .valueChanged)
+        tableView.addSubview(refreshControl!)
+    }
+    
+    func refresh() {
+        self.announcements = fetchAnnouncements()
+        self.tableView.reloadData()
+        
+        // Update core data objects with freshest data.
+        API.getAllAnnouncements() { announcements, success in
+            
+            if let announcements = announcements, success {
+                self.announcements.update(to: announcements, saveBlock: { announcement in
+                    print("Save event \(announcement.id)")
+                }, updateBlock: { from, to in
+                    print("Update event \(from.id)")
+                    from.title = to.title
+                    from.message = to.message
+                }, deleteBlock: { announcement in
+                    print("Delete event \(announcement.id)")
+                    AppDelegate.moc.delete(announcement)
+                }, completion: {
+                    try! AppDelegate.moc.save()
+                })
+            }
+            
+            // Refresh the local variable.
+            self.announcements = self.fetchAnnouncements()
+            self.refreshControl?.endRefreshing()
+            self.reload()
+        }
+    }
+    
+    func reload() {
+        sortedAnnouncements = announcements.sorted { lhs, rhs in
+            // Sort newest to oldest.
+            lhs.updatedAt > rhs.updatedAt
+        }
+        tableView.reloadData()
+    }
+    
+    func fetchAnnouncements() -> [Announcement] {
+        let fetchRequest = Announcement.fetchRequest()
+        let events = try? AppDelegate.moc.fetch(fetchRequest) as! [Announcement]
+        return events ?? []
+    }
+    
+    
+    // MARK: - Data Source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return sortedAnnouncements.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: nil)
+
+        cell.textLabel?.text = sortedAnnouncements[indexPath.row].title
+
+        return cell
+    }
+    
+}
