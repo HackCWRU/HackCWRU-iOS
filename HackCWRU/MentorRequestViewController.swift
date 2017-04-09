c93be0f89d43a8430e18219f8e4a19f75c1ebbd0
Jack Mousseau <jackmousseau@gmail.com>
2017-04-09 17:37:44 -0400

    Add MentorRequestViewController

--- HackCWRU/MentorRequestViewController.swift
+++ HackCWRU/MentorRequestViewController.swift
@@ -6,4 +6,49 @@
 //  Copyright © 2017 Hacker Society. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class MentorRequestViewController: UIViewController {
+    
+    @IBOutlet weak var menteeNameTextField: UITextField!
+    @IBOutlet weak var topicsTextField: UITextField!
+    @IBOutlet weak var locationDescriptionTextField: UITextField!
+    @IBOutlet weak var cancelButton: UIBarButtonItem!
+    @IBOutlet weak var submitMentorRequestButton: UIBarButtonItem!
+    
+    @IBAction func cancel(_ sender: UIBarButtonItem) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func submitMentorRequest(_ sender: UIBarButtonItem) {
+        guard let menteeName = menteeNameTextField.text else { return }
+        guard let topics = topicsTextField.text else { return }
+        guard let location = locationDescriptionTextField.text else { return }
+        
+        self.cancelButton.isEnabled = false
+        
+        let activityIndicator = UIActivityIndicatorView(frame: CGRect(x: 0, y: 0, width: 20, height: 20))
+        activityIndicator.color = .gray
+        navigationItem.setRightBarButton(UIBarButtonItem(customView: activityIndicator), animated: false)
+        activityIndicator.startAnimating()
+        
+        API.submitMentorRequest(menteeName: menteeName, topics: topics,
+                                locationDescription: location) { mentorRequest, succeeded in
+            if succeeded {
+                DispatchQueue.main.async {
+                    self.dismiss(animated: true, completion: nil)
+                }
+            } else {
+                DispatchQueue.main.async {
+                    self.cancelButton.isEnabled = true
+                    self.navigationItem.rightBarButtonItem = sender
+                }
+            }
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
